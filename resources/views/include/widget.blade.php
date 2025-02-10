76fa8b5db072bc681f1ffc71c13f08f7b7988aed
Vodila <vodila@servitor.solutions>
2025-02-10 16:25:52 +0200

    ep.51.admin-widgets

---
+++ resources/views/include/widget.blade.php
@@ -0,0 +1,6 @@
+<div class="card">
+    <div class="card-body">
+        <h4><span class="{{$icon ?? 'fas fa-user'}}"></span>{{$title ?? ''}}</h4>
+        <h2>{{$data ?? 0}}</h2>
+    </div>
+</div>
